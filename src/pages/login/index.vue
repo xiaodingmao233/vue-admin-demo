feb1479f63e1f1de112557fe0853a593f51fb03b
lizheng <2112276441@qq.com>
2021-04-12 08:56:41 +0800

    第一次提交

---
+++ src/pages/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    登录
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../pages/home'
+import Login from '../pages/login'
 
 Vue.use(VueRouter)
 
+// 初始化路由
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
+
+// 准备动态加载的路由
 
 const router = new VueRouter({
   mode: 'history',
